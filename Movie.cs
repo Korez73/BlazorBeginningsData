99dbfa51ba9093919b9fb7c5fb7af3cec8d290dc
Korez73 <jamie.weldon@outlook.com>
2025-07-14 11:18:49 -0500

    Add data annotations

--- Movie.cs
+++ Movie.cs
@@ -5,9 +5,13 @@ namespace BlazorBeginningsData.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [Required]
     public string? Title { get; set; }
     [DataType(DataType.Date)]
     public DateTime? ReleaseDate { get; set; }
     public string? Genre { get; set; }
+
+    [Range(0, 100)]
     public decimal Price { get; set; }
 }
