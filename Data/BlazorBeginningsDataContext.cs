c5a45d1c546ccd7b2ee54b2e3a4d796ec24c9035
Korez73 <jamie.weldon@outlook.com>
2025-07-14 05:46:43 -0500

    Scaffolding using EF and DataBinding with QuickGrid

---
+++ Data/BlazorBeginningsDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorBeginningsData.Models;
+
+namespace BlazorBeginningsData.Data
+{
+    public class BlazorBeginningsDataContext : DbContext
+    {
+        public BlazorBeginningsDataContext (DbContextOptions<BlazorBeginningsDataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorBeginningsData.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-using BlazorBeginningsData.Components;
+﻿using BlazorBeginningsData.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BlazorBeginningsData.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<BlazorBeginningsDataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlazorBeginningsDataContext") ?? throw new InvalidOperationException("Connection string 'BlazorBeginningsDataContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
